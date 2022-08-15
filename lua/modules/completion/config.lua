2b02a6f73647f2a12bec4b112dc899855152b2cd
Tal Vintrob <vintrob@cisco.com>
2022-08-15 16:52:34 +0300

    fixed tab error

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -1,18 +1,19 @@
 local config = {}
 
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
-end
-
-local feedkey = function(key, mode)
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-end
-
 function config.nvim_cmp()
   local cmp = require('cmp')
   local types = require('cmp.types')
   local mapping = require('cmp.config.mapping')
+
+  local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+  end
+
+  local feedkey = function(key, mode)
+    local keys = vim.api.nvim_replace_termcodes(key, true, true, true)
+    vim.api.nvim_feedkeys(keys, mode, true)
+  end
 
   cmp.setup({
     preselect = cmp.PreselectMode.Item,
