6692f5a8f6fd18bc83a3183d3b5b4f11d11ac694
tvaintrob <vintrob@cisco.com>
2023-08-26 22:15:20 +0300

    updated dashboard-nvim to use fzf-lua instead of telescope

--- lua/plugins/dashboard-nvim.lua
+++ lua/plugins/dashboard-nvim.lua
@@ -1,8 +1,21 @@
 return {
   'nvimdev/dashboard-nvim',
   event = 'VimEnter',
-  dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+  dependencies = {
+    { 'nvim-tree/nvim-web-devicons' },
+    { 'ibhagwan/fzf-lua' },
+  },
   config = function()
-    require('dashboard').setup()
+    require('dashboard').setup({
+      config = {
+        project = {
+          enable = true,
+          limit = 8,
+          action = function(path)
+            require('fzf-lua').files({ cwd = path })
+          end,
+        },
+      },
+    })
   end,
 }
