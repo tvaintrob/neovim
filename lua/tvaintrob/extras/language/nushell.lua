29ad28686ecde266dafe6e6ecaa72d491c74d066
tvaintrob <tal@harmony.so>
2025-06-28 22:35:42 +0300

    clean start

--- lua/tvaintrob/extras/language/nushell.lua
+++
@@ -1,8 +0,0 @@
-return {
-    {
-        'neovim/nvim-lspconfig',
-        config = function()
-            require('lspconfig').nushell.setup({})
-        end,
-    },
-}
--- lua/tvaintrob/plugins/indent-blankline.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'lukas-reineke/indent-blankline.nvim',
-    main = 'ibl',
-    event = 'LazyFile',
-    opts = {
-        scope = { enabled = false },
-        indent = { char = '▏', tab_char = '▏' },
-    },
-}
